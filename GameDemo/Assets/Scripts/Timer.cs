15b45020b27ba60e106e359cecfb90680f413678
LiamConroy <liamconroy2012@gmail.com>
2021-02-15 21:19:17 +0000

    Timer added, Spawn area added, kill boxes in correct positions

---
+++ GameDemo/Assets/Scripts/Timer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart;
+    public Text textBox;
+
+    bool timerActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox.text = timeStart.ToString("F2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerActive){
+        timeStart += Time.deltaTime;
+        textBox.text = timeStart.ToString("F2");
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+
+        Debug.Log(timerActive);
+
+        if (other.transform.CompareTag("Player")){
+            timerActive = !timerActive;
+        }
+    }
+
+}
